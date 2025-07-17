db772371c48e36818f6606f83905138fc517ad3b
Dead0G0D <52143116+Dead0G0D@users.noreply.github.com>
2025-07-17 02:58:45 -0300

    Update Anime_Storm.lua

--- Anime_Storm.lua
+++ Anime_Storm.lua
@@ -15,7 +15,7 @@ local Window = Fluent:CreateWindow({
 local Tabs = {
     Main = Window:AddTab({ Title = "Main", Icon = "sword" }),
     Trials = Window:AddTab({ Title = "Trial/Tower/Boss", Icon = "flame" }),
-    Summer = Window:AddTab({ Title = "SummerEvent", Icon = "Sun" }),
+    Summer = Window:AddTab({ Title = "SummerEvent", Icon = "sun" }),
     Misc = Window:AddTab({ Title = "Misc", Icon = "list" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
