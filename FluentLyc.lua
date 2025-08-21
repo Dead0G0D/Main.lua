9b64a9de14a999dc094f830507e6756331346a95
Dead0G0D <52143116+Dead0G0D@users.noreply.github.com>
2025-08-21 03:42:34 -0300

    Update FluentLyc.lua

--- FluentLyc.lua
+++ FluentLyc.lua
@@ -1845,7 +1845,7 @@ Components.Element = (function()
 		local Options = Options or {}
 
 		Element.TitleLabel = New("TextLabel", {
-			FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Medium, Enum.FontStyle.Normal),
+			FontFace = Font.new("rbxasset://fonts/families/HighwayGothic.json", Enum.FontWeight.Medium, Enum.FontStyle.Normal),
 			Text = Title,
 			TextColor3 = Color3.fromRGB(240, 240, 240),
 			TextSize = 13,
