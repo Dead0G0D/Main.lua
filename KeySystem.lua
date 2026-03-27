71d49d65da0f436a0b1072a1e250a4c836b72230
Dead0G0D <52143116+Dead0G0D@users.noreply.github.com>
2026-03-27 00:14:40 -0300

    Update KeySystem.lua

--- KeySystem.lua
+++ KeySystem.lua
@@ -10,7 +10,7 @@ if not game:IsLoaded() then game.Loaded:Wait() end
 -- ══════════════════════════════════════════
 
 local SUPPORTED_GAMES = {
-    [92783581681786] = "https://raw.githubusercontent.com/latencyhub59-sys/Script/refs/heads/main/Anime%20Clicker.lua",
+    [92783581681786] = "https://raw.githubusercontent.com/Dead0G0D/Main.lua/refs/heads/main/Anime%20Cicker%20(Hunters%202.0).lua",
     -- [PLACE_ID] = "https://raw.githubusercontent.com/.../script.lua",
 }
 
